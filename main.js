58dbbe3740915464ce6fdf195f71f8a5e0201451
Hunter1599 <87743543+Hunter1599@users.noreply.github.com>
2021-08-27 22:16:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var title =  document.getElementById('productsCart').getElementsByClassName('remover del carrito');
+title.innerText= "NEW TITLE";
+title.addEventListener("Click", function(){
+	alert("clicked");
+
+});
+console.log(title);
